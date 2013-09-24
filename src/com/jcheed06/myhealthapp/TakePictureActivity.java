715d20f67fa52826c7db96639332a63da23df45b
aelex13 <du.a.meijer@googlemail.com>
2013-09-24 14:50:23 +0200

    Take picture permission added

---
+++ src/com/jcheed06/myhealthapp/TakePictureActivity.java
@@ -0,0 +1,22 @@
+package com.jcheed06.myhealthapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TakePictureActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_take_picture);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.take_picture, menu);
+		return true;
+	}
+
+}
