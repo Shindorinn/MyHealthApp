8c3178f72dc1784149b78ad3a0d41480f11cbc3c
JJStoker <jeroen229@gmail.com>
2013-10-03 11:26:45 +0200

    should send all the measurement data now

--- src/com/jcheed06/myhealthapp/SendMeasurementTask.java
+++ src/com/jcheed06/myhealthapp/SendMeasurementTask.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
@@ -19,6 +20,7 @@ import org.json.JSONObject;
 
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.text.format.DateUtils;
 
 public class SendMeasurementTask extends BaseActivity {
 	public SharedPreferences sp;
@@ -42,7 +44,9 @@ public class SendMeasurementTask extends BaseActivity {
 						10);
 				nameValuePairs.add(new BasicNameValuePair("username", sp
 						.getString("username", "default")));
-
+				Date date = new Date();
+				nameValuePairs.add(new BasicNameValuePair("timestamp",
+						"" + date.getTime()));
 				if (params[0] instanceof PressureMeasurement) {
 					PressureMeasurement pm = (PressureMeasurement) params[0];
 					type = "0";
@@ -50,11 +54,35 @@ public class SendMeasurementTask extends BaseActivity {
 							"" + pm.getHyperTension()));
 					nameValuePairs.add(new BasicNameValuePair("hypotension", ""
 							+ pm.getHypoTension()));
-
+					
 				} else if (params[0] instanceof PulseMeasurement) {
 					PulseMeasurement psm = (PulseMeasurement) params[0];
+					type = "1";
+					nameValuePairs.add(new BasicNameValuePair("bpm",
+							"" + psm.getBPM()));
 				} else if (params[0] instanceof ECGMeasurement) {
 					ECGMeasurement ecgm = (ECGMeasurement) params[0];
+					type = "2";
+					nameValuePairs.add(new BasicNameValuePair("printerval",
+							"" + ecgm.getPrinterval()));
+					nameValuePairs.add(new BasicNameValuePair("prsegment",
+							"" + ecgm.getPrsegment()));
+					nameValuePairs.add(new BasicNameValuePair("qrscomplex",
+							"" + ecgm.getQrscomplex()));
+					nameValuePairs.add(new BasicNameValuePair("stsegment",
+							"" + ecgm.getStsegment()));
+					nameValuePairs.add(new BasicNameValuePair("qtinterval",
+							"" + ecgm.getQtinterval()));
+					nameValuePairs.add(new BasicNameValuePair("qtrough",
+							"" + ecgm.getQtrough()));
+					nameValuePairs.add(new BasicNameValuePair("rpeak",
+							"" + ecgm.getRpeak()));
+					nameValuePairs.add(new BasicNameValuePair("strough",
+							"" + ecgm.getStrough()));
+					nameValuePairs.add(new BasicNameValuePair("tpeak",
+							"" + ecgm.getTpeak()));
+					nameValuePairs.add(new BasicNameValuePair("ppeak",
+							"" + ecgm.getPpeak()));
 				}
 
 				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
